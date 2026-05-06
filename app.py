ddb20978423c1115d586c44e9dbce7bb46b918c9
Yash Mane <yashmane9872@gmail.com>
2026-05-06 09:34:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ HTML_TEMPLATE = """
                 <li>🐳 Docker: Ingress-> Load Balancer, Probes, Resource: Request and Limit, Multi-Stage Dockerfile</li>
                 <li>☸️ Prometheus  </li>
                 <li>📊 Grafana  </li>
-                <li>🤫 DevSecOps: GitLeaks  </li>
+                <li>🤫 DevSecOps: GitLeaks, SonarQube  </li>
                 <li>🤖 Jenkins: Shared Lib, Cred, Env Var, (IAM IRSA Integration)</li>
                 <li>☁️ AWS: EC2, RDS (IAM Auth Enabled), EKS (AWS Managed K8s)</li>
                 
