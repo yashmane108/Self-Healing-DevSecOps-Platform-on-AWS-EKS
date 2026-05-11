1588c81603ac7faec4c2c15639defcfbcdfdddff
Yash Mane <yashmane9872@gmail.com>
2026-05-11 10:58:06 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "demo_sg" {
+  name        = "terraform-demo-sg"
+  description = "Demo SG for drift testing"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "TerraformDemoSGG"
+  }
+}
