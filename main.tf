f18593759ba203fedce6e0cb49c9d78fc0cf24f5
Yash Mane <yashmane9872@gmail.com>
2026-05-11 11:07:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "demo_sg" {
   description = "Demo SG for drift testing"
 
   ingress {
-    from_ort   = 22
+    from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
